955341da23e17e0cc9e4eb4f794ea1e99ecba4ae
asytyi <alexander.a.sytyi@gmail.com>
2022-09-13 12:28:57 +0300

    Исправлена кодировка на UTF8

--- Firmware/Sources/Board/ZbBoard.h
+++ Firmware/Sources/Board/ZbBoard.h
@@ -1,4 +1,4 @@
-// -----------------------------------------
+﻿// -----------------------------------------
 // Declaration file for ZbBoard library for VTM-CB board family
 // 
 //		BASED ON 	- 	TI C/C++ Headers and Examples
