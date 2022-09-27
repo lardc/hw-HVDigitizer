4a478cd24253b43eeca2267e63bfa0e1b575ef6b
asytyi <alexander.a.sytyi@gmail.com>
2022-09-27 13:25:01 +0300

    Исправлены битовые маски для сдвигового регистра

--- Firmware/Sources/Controller/Global.h
+++ Firmware/Sources/Controller/Global.h
@@ -11,9 +11,9 @@
 
 // Definitions
 //
-#define OE_MASK_I1			BIT1	// Токовый шунт 1
-#define OE_MASK_I2			BIT2	// Токовый шунт 2
-#define OE_MASK_I3			BIT3	// Токовый шунт 3
+#define OE_MASK_I1			BIT0	// Токовый шунт 1
+#define OE_MASK_I2			BIT1	// Токовый шунт 2
+#define OE_MASK_I3			BIT2	// Токовый шунт 3
 
 #define SMPL_LED_BLINK		10		// Время вспышки светодиода получения посылок, мс
 
